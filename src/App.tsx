4af9ef7c47253d8b020225e040f9269e4b177b5c
odyildiz <ozgurdoganyildiz@hotmail..com>
2025-02-22 11:19:41 +0300

    add logout path

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import GalleryContent from './pages/GalleryContent';
 import MenuItems from './pages/MenuItems';
 import Home from './pages/Home';
 import { Login } from './pages/Login';
+import { Logout } from './pages/Logout';
 import { PrivateRoute } from './components/PrivateRoute';
 import { AuthProvider } from './contexts/AuthContext';
 
@@ -19,6 +20,7 @@ function App() {
           <div className="flex items-center justify-center mt-16">
             <Routes>
               <Route path="/login" element={<Login />} />
+              <Route path="/logout" element={<Logout />} />
               <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
               <Route path="/menu-content" element={<PrivateRoute><MenuContent /></PrivateRoute>} />
               <Route path="/gallery-content" element={<PrivateRoute><GalleryContent /></PrivateRoute>} />
